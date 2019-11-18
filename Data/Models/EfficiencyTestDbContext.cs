8e22d1e2ad23357e8b1d46ca7bce2f33931e6d43
Daniel Moskal <daniel19967@gmail.com>
2019-11-18 19:44:15 +0100

    fixed #3

--- Data/Models/EfficiencyTestDbContext.cs
+++ Data/Models/EfficiencyTestDbContext.cs
@@ -4,13 +4,12 @@ namespace Data.Models
 {
     public class EfficiencyTestDbContext : DbContext
     {
+        public EfficiencyTestDbContext(DbContextOptions<EfficiencyTestDbContext> options) : base(options) { }
+
         public virtual DbSet<TestParameters> TestsParameters { get; set; }
         public virtual DbSet<Test> Tests { get; set; }
         public virtual DbSet<Endpoint> Endpoints { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql(@"Server=efduaa.postgres.database.azure.com;Port = 5432;Database=EfficiencyTest;User Id = l01@efduaa;Password = PolitechnikaRzeszowska2019;");
-        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
     }
 }
