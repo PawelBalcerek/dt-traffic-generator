dfe5c3a764ff19b630bb505487502f57165567ba
Daniel Moskal <daniel19967@gmail.com>
2019-12-28 12:16:19 +0100

    Attachments (.csv, .zip) names for reports have been changed

--- TestLibrary/Configuration/Config.cs
+++ TestLibrary/Configuration/Config.cs
@@ -2,9 +2,10 @@
 {
     public static class Config
     {
-        public const string AverageEndpointsExecutionTimesCsvFileName = "averageExecutionTimes.csv";
-        public const string UserEndpointExecutionTimesCsvZipFileName = "userEndpointExecutionTimes.zip";
+        public const string AverageEndpointsExecutionTimesCsvFileName = "averageExecutionTimes";
+        public const string UserEndpointExecutionTimesCsvZipFileName = "userEndpointExecutionTimes";
         public const string CsvExtension = ".csv";
+        public const string ZipExtension = ".zip";
         public const string CsvContentType = "text/csv";
         public const string ZipContentType = "application/zip";
     }
